033f6458da31ef6d17c24cf8db2e610221ce8a98
kreamt <karimtabet@gmail.com>
2014-05-30 19:02:46 +0100

    parses json and adds to scope.results

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,12 +1,19 @@
 app.controller("mainController", function($scope, $http){
 
+    $scope.results = [];
     $scope.init = function() {
 
         //necessary for cross domain request
         delete $http.defaults.headers.common['X-Requested-With'];
-        
-        $http.get('https://spreadsheets.google.com/feeds/list/15oREoh6Csjn3cidwUcAczqbtZ882EwD1Qrh1jUnXKjc/od6/public/basic?hl=en_US&alt=json').success(function(data) {
-            console.log(data);
+
+        var url = 'https://spreadsheets.google.com/feeds/list/15oREoh6Csjn3cidwUcAczqbtZ882EwD1Qrh1jUnXKjc/od6/public/basic?hl=en_US&alt=json'
+        $http.get(url).success(function(data) {
+            var entry = data.feed.entry
+            angular.forEach(entry, function(content, index){
+                content.title = content.title.$t;
+                content.content = content.content.$t;
+                $scope.results.push(content);
+        });
         }).error(function(error) {
  
         });
