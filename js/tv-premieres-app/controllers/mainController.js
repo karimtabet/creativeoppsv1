70282cb62a50b3ca3d155bd70ede3f8e80da988c
kreamt <karimtabet@gmail.com>
2014-05-30 20:50:28 +0100

    scoping data and pulling in html

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,18 +1,23 @@
-app.controller("mainController", function($scope, $http){
-
-    $scope.results = [];
+app.controller("mainController", function($scope, $http) {
     $scope.init = function() {
-
         //necessary for cross domain request
         delete $http.defaults.headers.common['X-Requested-With'];
 
         var url = 'https://spreadsheets.google.com/feeds/list/15oREoh6Csjn3cidwUcAczqbtZ882EwD1Qrh1jUnXKjc/od6/public/basic?hl=en_US&alt=json'
         $http.get(url).success(function(data) {
             var entry = data.feed.entry
-            angular.forEach(entry, function(content, index){
-                content.title = content.title.$t;
-                content.content = content.content.$t;
-                $scope.results.push(content);
+            angular.forEach(entry, function(value, index) {
+                var content = value.content.$t.substring(9)
+                switch (value.title.$t) {
+                    case "Main Header":
+                        $scope.header = content;
+                    case "Home":
+                        $scope.home = content;
+                    case "Projects":
+                        $scope.projects = content;
+                    case "Contact":
+                        $scope.contact = content;
+                }
         });
         }).error(function(error) {
  
