586a26ca992bcd26df9edf3abab027dd41951c7a
kreamt <karimtabet@gmail.com>
2014-06-01 00:14:08 +0100

    nav highlighting based on scroll position with jquery

--- js/nav-highlights.js
+++
@@ -1,6 +0,0 @@
-function changeHighlighted(btnId) {
-    document.getElementById("homeBtn").className = "";
-    document.getElementById("projectsBtn").className = "";
-    document.getElementById("contactBtn").className = "";
-    document.getElementById(btnId).className = "pure-menu-selected";
-}
