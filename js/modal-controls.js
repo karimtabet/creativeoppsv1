aa67889d3f7411d133760dd159971d4256d907d0
kreamt <karimtabet@gmail.com>
2014-06-02 19:31:34 +0100

    plugs in owl carousel for photos in modal

--- js/modal-controls.js
+++ js/modal-controls.js
@@ -1,6 +1,15 @@
 function openModal(project) {
     var projectId = "#" + project;
     $(projectId).modal();
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:4,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoWidth:true,
+        lazyLoad:true
+    });
     return false;
 }
 
